4084ba2d05e62abfe0be8585152ff3cf367293a1
kamisama-anchor <1662669374@qq.com>
2024-09-27 21:11:06 +0800

    Update index3.js
    精简

--- 点名系统/pages/index3/index3.js
+++ 点名系统/pages/index3/index3.js
@@ -10,62 +10,16 @@ Page({
 
   onLoad: function() {
     this.readLatestRecord();
-    this.loadComments();
+
   },
   
-  bindCommentInput: function(e) {
-    // 绑定输入框的输入事件
-    this.setData({
-      comment: e.detail.value
-    });
-  },
+  
 
   clickBtn: function() {
     this.readLatestRecord();
-    this.loadComments();
   },
 
-  submitComment: function() {
-    // 提交评论的逻辑
-    const db = wx.cloud.database();
-    const _ = db.command;
-
-    if (this.data.comment.trim() === '') {
-      wx.showToast({
-        title: '评论不能为空',
-        icon: 'none'
-      });
-      return;
-    }
-
-    // 添加评论到数据库
-    db.collection('comments').add({
-      data: {
-        content: this.data.comment,
-        createTime: _.set(db.serverDate()), // 设置当前服务器时间
-        name:app.globalData.myname
-      },
-      success: res => {
-        console.log('评论添加成功', res);
-        wx.showToast({
-          title: '评论成功',
-          icon: 'success'
-        });
-        // 清空输入框
-        this.setData({
-          comment: ''
-        });
-      },
-      fail: err => {
-        console.error('评论添加失败', err);
-        wx.showToast({
-          title: '评论失败',
-          icon: 'none'
-        });
-      }
-    });
-  },
-
+ 
   readLatestRecord: function() {
     const db = wx.cloud.database(); // 获取数据库的引用
     const users = db.collection('users'); // 获取users集合的引用
