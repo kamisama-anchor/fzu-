98f99b519742b597c968b11c762463b2e475d8da
faquuuuu1 <153807118+faquuuuu1@users.noreply.github.com>
2024-09-29 16:08:41 +0800

    Update comment.js
    增加了可以显示的评论量

--- 点名系统/pages/comment/comment.js
+++ 点名系统/pages/comment/comment.js
@@ -4,11 +4,12 @@ Page({
   data: {
     latestRecord: null, // 使用null初始化
     comment: '',
-    comments: []
+    comments: [],
+    hasMoreComments: true, // 是否还有更多评论
   },
 
   onLoad: function() {
-    this.loadComments();
+    this.loadMoreComments();
   },
   
   bindCommentInput: function(e) {
@@ -19,7 +20,7 @@ Page({
   },
 
   clickBtn: function() {
-    this.loadComments();
+    this.loadMoreComments();
   },
 
   submitComment: function() {
@@ -39,8 +40,8 @@ Page({
     db.collection('comments').add({
       data: {
         content: this.data.comment,
-        createTime: _.set(db.serverDate()), // 设置当前服务器时间
-        name:app.globalData.myname
+        createTime: db.serverDate(), // 设置当前服务器时间
+        name: app.globalData.myname
       },
       success: res => {
         console.log('评论添加成功', res);
@@ -52,6 +53,8 @@ Page({
         this.setData({
           comment: ''
         });
+        // 重新加载评论
+        this.loadMoreComments();
       },
       fail: err => {
         console.error('评论添加失败', err);
@@ -63,22 +66,42 @@ Page({
     });
   },
 
-  loadComments: function() {
+  loadMoreComments: function() {
     const db = wx.cloud.database();
-    // 按创建时间降序排序
-    db.collection('comments').orderBy('createTime', 'desc').get({
-      success: res => {
-        if (res.data.length > 0) {
-          this.setData({
-            comments: res.data
-          });
-        } else {
-          console.log('没有查询到评论');
+    let that = this;
+    let pageSize = 20; // 每页数据量
+    let page = 0; // 当前页码
+    let allComments = this.data.comments; // 存储所有评论
+
+    function getData() {
+      db.collection('comments').orderBy('createTime', 'desc').skip(page * pageSize).limit(pageSize).get({
+        success: res => {
+          console.log('评论', res.data);
+          if (res.data.length > 0) {
+            allComments = allComments.concat(res.data); // 将新获取的评论添加到数组中
+            that.setData({
+              comments: allComments
+            });
+            page++;
+            if (res.data.length < pageSize) {
+              // 如果返回的数据少于pageSize，说明已经获取完所有评论
+              that.setData({
+                hasMoreComments: false
+              });
+            }
+          } else {
+            console.log('没有查询到更多评论');
+            that.setData({
+              hasMoreComments: false
+            });
+          }
+        },
+        fail: err => {
+          console.error('查询失败：', err);
         }
-      },
-      fail: err => {
-        console.error('查询失败：', err);
-      }
-    });
+      });
+    }
+
+    getData(); // 调用函数开始获取评论
   }
 });
