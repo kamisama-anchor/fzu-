f5d15bacda4d3aa87cad3f2a14c917ebe37aa299
faquuuuu1 <153807118+faquuuuu1@users.noreply.github.com>
2024-09-29 16:09:38 +0800

    Update rankk.js
    排行榜增加显示数量

--- 点名系统/pages/rankk/rankk.js
+++ 点名系统/pages/rankk/rankk.js
@@ -2,7 +2,9 @@
 const db = wx.cloud.database({
   env: 'software-9g3qgled9f6190ea'
 })
+
 const app = getApp();
+
 Page({
   data: {
     records: [],
@@ -23,20 +25,42 @@ onPullDownRefresh: function () { //下拉刷新
     wx.stopPullDownRefresh();
     this.clickBtn();
 },
-  clickBtn(){
-    const that = this;
-  db.collection('users').get({
-    success: function(res) {
-      // res.data 是一个包含集合中有权限访问的所有记录的数据，不超过 20 条
-      console.log(res.data);
-      const sortedRecords = res.data.sort(function(a, b) {
-        return b.score - a.score;
-      });
-      that.setData({
-        records: res.data
-      });
-    }
-  })
+clickBtn() {
+  const that = this;
+  let pageSize = 20; // 每页数据量
+  let page = 0; // 当前页码
+  let allData = []; // 存储所有数据
+
+  function getData() {
+    db.collection('users').skip(page * pageSize).limit(pageSize).get({
+      success: function(res) {
+        console.log(res.data);
+        allData = allData.concat(res.data); // 将新获取的数据添加到数组中
+        that.setData({
+          records: allData
+        });
+
+        if (res.data.length < pageSize) {
+          // 如果返回的数据少于pageSize，说明已经获取完所有数据
+          const sortedRecords = allData.sort(function(a, b) {
+            return b.score - a.score;
+          });
+          that.setData({
+            records: sortedRecords
+          });
+        } else {
+          // 否则，继续获取下一页数据
+          page++;
+          getData();
+        }
+      },
+      fail: function(err) {
+        console.error(err);
+      }
+    });
+  }
+
+  getData(); // 调用函数开始获取数据
 },
 inputChange: function(e) {
   this.setData({
